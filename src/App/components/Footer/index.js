e4368e3cdb4505519122778f231a5b22d176bf01
Ahmad Forhat <ahmad.forhat@gmail.com>
2020-06-12 01:45:30 -0300

    footer component

---
+++ src/App/components/Footer/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+    return (
+        <footer style={{backgroundColor:'black', color:'white',width:'100%',height:'40px', display:'flex', justifyContent:'center',textAlign:'center',position:'fixed', bottom: 0}}>
+            <p style={{fontSize:'10px', alignSelf:'center'}}>Todos direitos reservados a Midia.Tools</p>
+        </footer>
+    )
+  }
+
+export default Footer
