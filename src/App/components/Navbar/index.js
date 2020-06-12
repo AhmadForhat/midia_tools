41f117416ca1a8f4baa3f762076f00ea949db6be
Ahmad Forhat <ahmad.forhat@gmail.com>
2020-06-12 01:46:14 -0300

    change nav position and style

--- src/App/components/Navbar/index.js
+++ src/App/components/Navbar/index.js
@@ -2,38 +2,34 @@ import React ,{useState} from "react";
 import {
     Link
   } from "react-router-dom";
-import {container} from './style'
+import {container, linkStyle} from './style'
 import exitIcon from './image/clear-24px.svg'
 
 function Navbar({title}) {
     const [hamburger, setHamburger] = useState('false')
     return (
         <>
-        <nav style={{display:'flex', marginTop:'20px', marginLeft:'40px'}}>
-            {hamburger &&
-            <div>
-                <a onClick={() => {hamburger ? setHamburger(false) : setHamburger(true)}}>
-                    <img src="https://cdn4.iconfinder.com/data/icons/wirecons-free-vector-icons/32/menu-alt-512.png" alt="Menu logo" style={{width:'30px', marginTop:'10px'}}/>
-                </a>
+                {hamburger &&
+                        <a style={{marginLeft:'20px', width: '200px', position: 'fixed'}} onClick={() => {hamburger ? setHamburger(false) : setHamburger(true)}}>
+                            <img src="https://cdn4.iconfinder.com/data/icons/wirecons-free-vector-icons/32/menu-alt-512.png" alt="Menu logo" style={{width:'30px', marginTop:'10px'}}/>
+                        </a>
+                }
+                {!hamburger && 
+                    <nav style={container}>
+                            <a style={{marginLeft:'20px'}} onClick={() => {hamburger ? setHamburger(false) : setHamburger(true)}}>
+                                <img src={exitIcon} style={{width:'30px', marginTop:'0.8rem'}} alt="exit menu logo"/>
+                            </a>
+                            <div style={{display:'flex', flexDirection:'column', textAlign:'center'}}>
+                                <Link style={linkStyle} to="/">Home</Link>
+                                <Link style={linkStyle} to="/about">About</Link>
+                                <Link style={linkStyle} to="/users">Users</Link>
+                                <Link style={linkStyle} to="/login">Login</Link>
+                            </div>
+                    </nav>
+                }
+            <div style={{display:'flex', justifyContent:'center'}}>
+                <h2>{title}</h2>
             </div>
-            }
-            {!hamburger &&
-                <>
-                    <a onClick={() => {hamburger ? setHamburger(false) : setHamburger(true)}}>
-                        <img src={exitIcon} style={{width:'30px', marginTop:'10px'}} alt="exit menu logo"/>
-                    </a>
-                    <div style={container}>
-                        <Link style={{textDecoration:'none', color:'200B5C', marginTop:'20px'}} to="/home">Home</Link>
-                        <Link style={{textDecoration:'none', color:'200B5C', marginTop:'40px'}} to="/about">About</Link>
-                        <Link style={{textDecoration:'none', color:'200B5C', marginTop:'40px'}} to="/users">Users</Link>
-                        <Link style={{textDecoration:'none', color:'200B5C', marginTop:'40px', marginBottom:'20px'}} to="/login">Login</Link>
-                    </div>
-                </>
-            }
-        </nav>
-        <div>
-            <h2 style={{display:'flex', justifyContent:'center'}}>{title}</h2>
-        </div>
         </>
     )
   }
--- src/App/components/Navbar/style.js
+++ src/App/components/Navbar/style.js
@@ -1,10 +1,18 @@
 export const
     container = {
         overflow: 'hidden',
+        position: 'fixed',
         display:'flex',
         flexDirection: 'column',
-        textAlign:'center',
-        position: 'fixed',
-        top: '60px',
-        width: '20%'
+        textAlign:'left',
+        height:'100%',
+        width: '200px',
+        zIndex:'200',
+        backgroundColor:'#FFFBFC',
+        shadow:'2px 2px 2px 1px #FAEAFF'
+    },
+    linkStyle = {
+        textDecoration:'none',
+        color:'#200B5C',
+        marginTop:'40px'
     }
