3b2fe4de9befad57cf4ee7fd60583666140f6fe6
Ahmad Forhat <ahmad.forhat@gmail.com>
2020-06-12 01:45:52 -0300

    card component

---
+++ src/App/components/Card/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import {container, titulo, caixa} from './style'
+
+function Card() {
+    return (
+        <div>
+            
+        </div>
+    )
+  }
+
+export default Card
