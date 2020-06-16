bff60e77e84f0fb737345a1728cdcf4b2f402a78
Ahmad Forhat <ahmad.forhat@gmail.com>
2020-06-16 00:26:25 -0300

    render home components

--- src/App/pages/Home/index.js
+++ src/App/pages/Home/index.js
@@ -1,60 +1,54 @@
-import React, {useState, useEffect} from "react";
+import React, {useState, useEffect} from "react"
 import fetch from './fetch'
 import Slide from '../../components/Slide'
 import Footer from '../../components/Footer'
-import {mapa} from './style'
+import Card from '../../components/Card'
+import Devs from '../../components/Devs'
+import Contato from '../../components/Contato'
 
 function Home() {
   const [user, setUser] = useState('')
   const [info, setInfo] = useState('')
   const state = {user, info, setUser, setInfo}
     useEffect(() => fetch(state), [state])
-    const table = ['Oi', 'Tchau', 'Batata', 'Mud',"Melão", "Abacate"]
+    const table = [
+    <Card title='Serviços' img='https://images.unsplash.com/photo-1454165804606-c3d57bc86b40?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80' texto='Criamos soluções para a sua empresa, com foco em facilitar e dinamizar suas redes sociais.' />,
+    <Card title='Missões' img='https://images.unsplash.com/photo-1543286386-2e659306cd6c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80' texto='Criamos soluções para a sua empresa, com foco em facilitar e dinamizar suas redes sociais.' />,
+    <Card title='Equipe' img='https://images.unsplash.com/photo-1522071820081-009f0129c71c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80' texto='Criamos soluções para a sua empresa, com foco em facilitar e dinamizar suas redes sociais.' />
+  ]
     const arrayDevs = [
       {
         nome:'AhmadForhat',
-        urlImg:'https://avatars3.githubusercontent.com/u/54677103?s=400&u=b6e4e11c6718162d380a34d127626d0ce775414a&v=4'
+        urlImg:'https://avatars3.githubusercontent.com/u/54677103?s=400&u=b6e4e11c6718162d380a34d127626d0ce775414a&v=4',
+        urlGit:'https://github.com/AhmadForhat'
+      },
+      {
+        nome:'Letpmartins',
+        urlImg:'https://avatars1.githubusercontent.com/u/54192824?s=400&v=4',
+        urlGit:'https://github.com/Letpmartins'
+      },
+      {
+        nome:'mayvlima',
+        urlImg:'https://avatars3.githubusercontent.com/u/60523960?s=400&v=4',
+        urlGit:'https://github.com/mayvlima'
+      },
+      {
+        nome:'pcr0',
+        urlImg:'https://avatars0.githubusercontent.com/u/58571599?s=400&u=0a1809ce5f023e4f23c4b61641510eae11de046e&v=4',
+        urlGit:'https://github.com/pcr0'
       },
       {
         nome:'AhmadForhat',
-        urlImg:'https://avatars3.githubusercontent.com/u/54677103?s=400&u=b6e4e11c6718162d380a34d127626d0ce775414a&v=4'
-      },
-      {
-        nome:'AhmadForhat',
-        urlImg:'https://avatars3.githubusercontent.com/u/54677103?s=400&u=b6e4e11c6718162d380a34d127626d0ce775414a&v=4'
-      },
-      {
-        nome:'AhmadForhat',
-        urlImg:'https://avatars3.githubusercontent.com/u/54677103?s=400&u=b6e4e11c6718162d380a34d127626d0ce775414a&v=4'
-      },
-      {
-        nome:'AhmadForhat',
-        urlImg:'https://avatars3.githubusercontent.com/u/54677103?s=400&u=b6e4e11c6718162d380a34d127626d0ce775414a&v=4'
+        urlImg:'https://avatars3.githubusercontent.com/u/54677103?s=400&u=b6e4e11c6718162d380a34d127626d0ce775414a&v=4',
+        urlGit:'https://github.com/AhmadForhat'
       }
     ]
     return (
       <>
-      <Slide table ={table}/>
-      <div className="devs" style={{marginTop:'50px', display:'flex', flexDirection:'column', width:'100%', height:'100%',marginBottom: '40px'}}>
-        <h2 style={{marginLeft:'20px'}}>Devs</h2>
-        <div style={{display:'flex', justifyContent:'space-around', flexWrap: 'wrap'}}>
-          {arrayDevs.map(dev => {
-            return (
-              <>
-              <div style={{display:'flex', flexDirection:'column', textAlign:'center', justifyContent:'center'}}>
-                <h3>{dev.nome}</h3>
-                <img style={{borderRadius:'400px', width:'150px', alignSelf:'center'}} alt={`imagem do ${dev.name}`} src={dev.urlImg}/>
-              </div>
-              </>
-            )
-          })}
-        </div>
-      </div>
-      <div className="contato" style={{marginTop:'50px', display:'flex', flexDirection:'column', height:'400px'}}>
-        <h2 style={{marginLeft:'20px'}}>Contato</h2>
-        <iframe title="local dh" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3656.047909215844!2d-46.67753668502145!3d-23.602614684662434!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce50ab7feb4519%3A0x739f0ddb0439cf94!2sDigital%20House%20S%C3%A3o%20Paulo!5e0!3m2!1spt-BR!2sbr!4v1591933080144!5m2!1spt-BR!2sbr" style={mapa}></iframe>
-      </div>
-      <Footer />
+        <Slide table={table}/>
+        <Devs arrayDevs={arrayDevs} />
+        <Contato />
+        <Footer />
       </>
     )
   }
