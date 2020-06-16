2376c3669ac655ca600f3594343f485591191818
Ahmad Forhat <ahmad.forhat@gmail.com>
2020-06-16 00:25:02 -0300

    card component

--- src/App/components/Card/index.js
+++ src/App/components/Card/index.js
@@ -1,11 +1,14 @@
 import React from "react";
-import {container, titulo, caixa} from './style'
 
-function Card() {
+function Card({title, img, texto}) {
     return (
-        <div>
-            
-        </div>
+        <body style={{display:'flex', flexWrap: 'wrap'}}>
+              <img style={{width:'30%', minWidth:'180px', heigth:'100%', margin: '0 auto'}} alt={`imagem sobre ${title}`} src={img}/>
+            <div style={{marginLeft:'20px', textAlign:'center',maxWidth:'65%', minWidth:'180px', margin:'0 auto'}}>
+              <h2>{title}</h2>
+              <p>{texto}</p>
+            </div>
+        </body>
     )
   }
 
