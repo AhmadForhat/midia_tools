5ad74a737a092aa6dd15de3986f016d62c4d62c4
Ahmad Forhat <ahmad.forhat@gmail.com>
2020-06-16 16:45:26 -0300

    contato info home

--- src/App/pages/Home/index.js
+++ src/App/pages/Home/index.js
@@ -23,27 +23,32 @@ function Home() {
       {
         nome:'AhmadForhat',
         urlImg:'https://avatars3.githubusercontent.com/u/54677103?s=400&u=b6e4e11c6718162d380a34d127626d0ce775414a&v=4',
-        urlGit:'https://github.com/AhmadForhat'
+        urlGit:'https://github.com/AhmadForhat',
+        info:'Desenvolvedor JavaScript'
       },
       {
         nome:'Letpmartins',
         urlImg:'https://avatars1.githubusercontent.com/u/54192824?s=400&v=4',
-        urlGit:'https://github.com/Letpmartins'
+        urlGit:'https://github.com/Letpmartins',
+        info:'Desenvolvedor FullStack'
       },
       {
         nome:'mayvlima',
         urlImg:'https://avatars3.githubusercontent.com/u/60523960?s=400&v=4',
-        urlGit:'https://github.com/mayvlima'
+        urlGit:'https://github.com/mayvlima',
+        info:'Desenvolvedor FullStack'
       },
       {
         nome:'pcr0',
         urlImg:'https://avatars0.githubusercontent.com/u/58571599?s=400&u=0a1809ce5f023e4f23c4b61641510eae11de046e&v=4',
-        urlGit:'https://github.com/pcr0'
+        urlGit:'https://github.com/pcr0',
+        info:'Desenvolvedor FullStack'
       },
       {
         nome:'AhmadForhat',
         urlImg:'https://avatars3.githubusercontent.com/u/54677103?s=400&u=b6e4e11c6718162d380a34d127626d0ce775414a&v=4',
-        urlGit:'https://github.com/AhmadForhat'
+        urlGit:'https://github.com/AhmadForhat',
+        info:'Desenvolvedor FullStack'
       }
     ]
     return (
