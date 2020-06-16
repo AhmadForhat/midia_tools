37978a0278e8a7a953a0cddf91ca3b562890df24
Ahmad Forhat <ahmad.forhat@gmail.com>
2020-06-16 16:44:18 -0300

    change color footer

--- src/App/components/Footer/index.js
+++ src/App/components/Footer/index.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function Footer() {
     return (
-        <footer style={{backgroundColor:'black', color:'white',width:'100%',height:'40px', display:'flex', justifyContent:'center',textAlign:'center',position:'fixed', bottom: 0}}>
+        <footer style={{backgroundColor:'#11023C', color:'white',width:'100%',height:'40px', display:'flex', justifyContent:'center',textAlign:'center',position:'fixed', bottom: 0}}>
             <p style={{fontSize:'10px', alignSelf:'center'}}>Todos direitos reservados a Midia.Tools</p>
         </footer>
     )
