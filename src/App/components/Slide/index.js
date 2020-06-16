4afd273d3a05f671df4b401f658603e0af6f8e9a
Ahmad Forhat <ahmad.forhat@gmail.com>
2020-06-16 00:26:14 -0300

    slide component

--- src/App/components/Slide/index.js
+++ src/App/components/Slide/index.js
@@ -7,24 +7,22 @@ function Slide({table}) {
     const [slide, setSlide] = useState(0)
     return (
         <>
-        <div style={{display:'flex', flexDirection:'column', marginTop:'40px'}}>
+        <body style={{display:'flex', flexDirection:'column', marginTop:'40px'}}>
         <div style={{display:'flex', justifyContent:'space-between'}}>
-        <img style={{marginLeft:'40px'}} alt='>' src={esquerda} onClick={() => {
+        <img style={{marginLeft:'5%', width:'20px', height:'40px', alignSelf:'center', marginRight:'5%'}} alt='>' src={esquerda} onClick={() => {
             if(slide !== 0) setSlide(slide - 1)
             }}/>
-        <div>
             {table[slide]}
-        </div>
-        <img style={{marginRight:'40px'}} alt='>' src={direita} onClick={() => {
+        <img style={{marginRight:'5%', width:'20px', height:'40px', alignSelf:'center', marginLeft:'5%'}} alt='>' src={direita} onClick={() => {
             if(slide !== table.length - 1) setSlide(slide + 1)
             }}/>
         </div>
-        <div style={{display:'flex', justifyContent:'space-around',margin:'0 auto', maxWidth:'200px', marginTop:'35px'}}>
+        <div style={{display:'flex', justifyContent:'space-around',margin:'0 auto', maxWidth:'200px', marginTop:'35px',}}>
             {table.map((item,index) => {
                 return <button key={index} style={slide === index ? index === 0 ? firstActiveButton: activeButton : index === 0 ? fistButton : button} onClick={() => setSlide(index)}/>
             })}
         </div>
-        </div>
+        </body>
         </>
     )
   }
--- src/App/components/Slide/style.js
+++ src/App/components/Slide/style.js
@@ -1,9 +1,9 @@
 export const
     button = {padding:'12px', backgroundColor:'black', borderRadius:'100px', outline: 'none',
     border:'none', marginLeft:'20px'},
-    activeButton = {padding:'12px', backgroundColor:'green', borderRadius:'100px', outline: 'none',
+    activeButton = {padding:'12px', backgroundColor:'#371C85', borderRadius:'100px', outline: 'none',
     border:'none', marginLeft:'20px'},
     fistButton = {padding:'12px', backgroundColor:'black', borderRadius:'100px', outline: 'none',
     border:'none'},
-    firstActiveButton = {padding:'12px', backgroundColor:'green', borderRadius:'100px', outline: 'none',
+    firstActiveButton = {padding:'12px', backgroundColor:'#371C85', borderRadius:'100px', outline: 'none',
     border:'none'}
