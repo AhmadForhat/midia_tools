9c3feeaa88de4ae58e753a5252f4baa777a4fe45
Ahmad Forhat <ahmad.forhat@gmail.com>
2020-06-16 16:43:33 -0300

    contato style

--- src/App/components/Contato/style.js
+++ src/App/components/Contato/style.js
@@ -1,3 +1,3 @@
 export const
-mapa = {width:'90%', height:"220px", frameborder:"0",border:"0", allowfullscreen:"",
+mapa = {width:'96%', height:"220px", frameborder:"0",border:"0", allowfullscreen:"",
 tabindex:"0", ariaHidden:"false", tabIndex:"0", margin:'0 auto'}
