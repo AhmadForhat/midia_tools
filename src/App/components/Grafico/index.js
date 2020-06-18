24f9a9ce33cc9c2e2c799d27a0163286fe0bf853
Ahmad Forhat <ahmad.forhat@gmail.com>
2020-06-17 21:16:30 -0300

    change graphic useMemo

--- src/App/components/Grafico/index.js
+++ src/App/components/Grafico/index.js
@@ -3,47 +3,14 @@ import { Chart } from 'react-charts'
 
 
 function Grafico({dados}) {
-
-    const data = React.useMemo(
-        () => [
-          {
-            label: 'Geral',
-            data: dados
-          }
-        ],
-        []
-      )
-      const series = React.useMemo(
-        () => ({
-          type: 'bar'
-        }),
-        []
-      )
-      const axes = React.useMemo(
-        () => [
-          { primary: true, type: 'ordinal', position: 'left' },
-          { position: 'bottom', type: 'linear', stacked: true }
-        ],
-        []
-      )
-      const getSeriesStyle = React.useCallback(
-        () => ({
-          transition: 'all .5s ease'
-        }),
-        []
-      )
-      const getDatumStyle = React.useCallback(
-        () => ({
-          transition: 'all .5s ease'
-        }),
-        []
-      )
-
+    const data = [{label: 'Geral',data: dados}]
+    const series ={type: 'bar'}
+    const axes =[{ primary: true, type: 'ordinal', position: 'left' },{ position: 'bottom', type: 'linear', stacked: true }]
+    
      return(
          <>
         <div style={{width: '500px',height: '300px'}}>
-        <Chart data={data} series={series} axes={axes} getSeriesStyle={getSeriesStyle}
-          getDatumStyle={getDatumStyle} />
+        <Chart data={data} series={series} axes={axes} />
         </div>
         </>
      )
