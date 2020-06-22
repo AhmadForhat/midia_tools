6d961b8b3fffa8c2bc6218676158ee87f18d5a3a
Ahmad Forhat <ahmad.forhat@gmail.com>
2020-06-22 18:04:52 -0300

    change button

--- src/App/components/Button/index.js
+++ src/App/components/Button/index.js
@@ -3,10 +3,10 @@ import {butao, butaoClick, butaoSmall} from './style'
 
 function Button({type, texto, click, small}) {
       if(click && !small){
-        return <button style={butaoClick} onClick={click}>{texto.toUpperCase()}</button>
+        return <button style={butaoClick} type={'submit'} onClick={click}>{texto.toUpperCase()}</button>
       }
       if(small){
-        return <button style={butaoSmall} onClick={click}>{texto.toUpperCase()}</button>
+        return <button style={butaoSmall} type={'submit'} onClick={click}>{texto.toUpperCase()}</button>
       }
       else{
         return <button style={butao} type={type}>{texto.toUpperCase()}</button>
