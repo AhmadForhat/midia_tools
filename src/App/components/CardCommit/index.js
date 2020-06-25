041ed9c5857e8f3b8f8c40d406e23c39861bfb10
Ahmad Forhat <ahmad.forhat@gmail.com>
2020-06-25 03:20:20 -0300

    error params not found

--- src/App/components/CardCommit/index.js
+++ src/App/components/CardCommit/index.js
@@ -1,5 +1,7 @@
 import React from "react";
 
+const avatar_fake = 'https://github.githubassets.com/images/modules/logos_page/Octocat.png'
+
 function CardCommit({data}) {
     
     
@@ -7,7 +9,7 @@ function CardCommit({data}) {
          <div style={{display: "flex", width:"100%", flexWrap:'wrap', minWidth:'300px', marginBottom:'40px'}}>
              {data.map(item =>  (
                 <body style={{width: '20%', minWidth: '220px', display: 'flex', flexDirection: 'column', padding: '20px', backgroundColor: 'white', boxShadow:'0 2px 4px 1px #B3B3B3', margin: '20px auto'}}>
-                <img src={item.avatar_url} alt="imagem usuario" style={{margin: "0 auto", width:"30%", minWidth:"180px", borderRadius:'200px'}}/>
+                <img src={item.avatar_url ? item.avatar_url : avatar_fake } alt="imagem usuario" style={{margin: "0 auto", width:"30%", minWidth:"180px", borderRadius:'200px'}}/>
                 <a href={item.html_url} target= 'blank' ><h2 style={{textAlign: 'center'}}>{item.login} </h2></a>
                    <div style={{display: 'flex', flexDirection:'column', justifyContent:'flex-start', width:'70%' }}>
                     <p style={{margin:'0'}}>Commits: {item.contributions}</p>
--- src/App/components/Commits/index.js
+++ src/App/components/Commits/index.js
@@ -1,18 +1,17 @@
 import React from "react";
 import moment from "moment"
 
+const avatar_fake = 'https://github.githubassets.com/images/modules/logos_page/Octocat.png'
+const login_fake = 'GitHub Undefined'
+
 function Teste({dados}) {
     
     
     return(
          <div style={{display: "flex", width:"100%", flexWrap:'wrap', minWidth:'300px', justifyContent:'center'}}>
              {dados.map(item =>  (
-                <body style={{width: '40%', height:'150px', display: 'flex', padding: '20px', backgroundColor: 'white', boxShadow:'0 2px 4px 1px #B3B3B3', margin:'10px', justifyContent:'center', alignItems:'center', alignContent:'center'}}>
-                    <img src={item.author.avatar_url} alt="imagem usuario" style={{width:"24%", borderRadius:'400px', alignSelf:'end'}}/>
-                    <div style={{display:'flex', flexDirection:'column', justifyContent:'center', alignItems:'center', alignContent:'center'}}>
-                    <a href={item.html_url}><h2 style={{textAlign: 'center'}}>{item.author.login}</h2></a>                                      
-                    <a href={item.html_url} target= 'blank' ><p style={{textAlign: 'center'}}>{item.commit.message.substr(0,55)+"..."}</p></a>                    
-                    <p style={{textAlign: 'center'}}>{moment(item.commit.author.date).format('DD/MMM/YYYY, LT')}</p>
+                    <img src={item.author ? item.author.avatar_url : avatar_fake} alt="imagem usuario" style={{width:"24%", minWidth:'150px', borderRadius:'400px', margin:'0 auto'}}/>
+                    <a href={item.html_url}><h2>{item.author ? item.author.login : login_fake}</h2></a>                                      
                     </div>                                  
                 </body>
              ))}        
